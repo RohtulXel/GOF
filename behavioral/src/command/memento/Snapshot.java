c46c56b1a350b3eb7b276e164016f048c7d1593b
RohtulXel <polilex789@gmail.com>
2020-03-24 17:16:22 +0200

    Add comment about memento implementation

--- behavioral/src/command/memento/Snapshot.java
+++ behavioral/src/command/memento/Snapshot.java
@@ -7,6 +7,7 @@ public class Snapshot {
     private TextFile textFile;
 
     public Snapshot(TextFile textFile) {
+        //almost memento :) in classic perspective it way to restore state of ordinato object, but you create new one
         this.textFile = new TextFile(textFile.getContext());
     }
 
